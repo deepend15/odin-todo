ecdc13cef7436eeefd9feed0b880c247a231ff7e
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-02-25 14:15:52 -0500

    Further style nav project buttons

--- src/index.js
+++ src/index.js
@@ -4,27 +4,52 @@ import { loadWork } from "./work-page.js";
 import { loadHobbies } from "./hobbies-page.js";
 import { loadGrocery } from "./grocery-page.js";
 
-loadPersonal();
-
 const personalBtn = document.querySelector(".personal-btn");
 const workBtn = document.querySelector(".work-btn");
 const hobbiesBtn = document.querySelector(".hobbies-btn");
 const groceryBtn = document.querySelector(".grocery-btn");
 
-personalBtn.addEventListener("click", loadPersonal);
-workBtn.addEventListener("click", loadWork);
-hobbiesBtn.addEventListener("click", loadHobbies);
-groceryBtn.addEventListener("click", loadGrocery);
+const projectButtonWrappers = document.querySelectorAll(".project-button-wrapper");
 
+loadPersonal();
+personalBtn.parentElement.classList.add("project-button-selected");
 
-function toggleNavButtonHighlighted(e) {
-    e.target.classList.toggle("nav-button-highlighted");
+function unselectCurrentProject() {
+    projectButtonWrappers.forEach((wrapper) => {
+        wrapper.classList.remove("project-button-selected");
+    });
 }
 
-const navButtonWrappers = document.querySelectorAll(".nav-button-wrapper");
+personalBtn.parentElement.addEventListener("click", () => {
+    loadPersonal();
+    unselectCurrentProject();
+    personalBtn.parentElement.classList.add("project-button-selected");
+});
 
-navButtonWrappers.forEach((wrapper) => {
-    wrapper.addEventListener("mouseenter", toggleNavButtonHighlighted);
-    wrapper.addEventListener("mouseleave", toggleNavButtonHighlighted);
+workBtn.parentElement.addEventListener("click", () => {
+    loadWork();
+    unselectCurrentProject();
+    workBtn.parentElement.classList.add("project-button-selected");
+});
+
+hobbiesBtn.parentElement.addEventListener("click", () => {
+    loadHobbies();
+    unselectCurrentProject();
+    hobbiesBtn.parentElement.classList.add("project-button-selected");
+});
+
+groceryBtn.parentElement.addEventListener("click", () => {
+    loadGrocery();
+    unselectCurrentProject();
+    groceryBtn.parentElement.classList.add("project-button-selected");
+});
+
+function toggleProjectButtonHighlighted(e) {
+    e.target.classList.toggle("project-button-highlighted");
+}
+
+projectButtonWrappers.forEach((wrapper) => {
+    wrapper.addEventListener("mouseenter", toggleProjectButtonHighlighted);
+    wrapper.addEventListener("mouseleave", toggleProjectButtonHighlighted);
 });
 
