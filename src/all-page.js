0355946384bdcf7d60c43d39c2e49f0a5bbd7635
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-03-13 19:24:28 -0400

    Add all todos to 'all todos' page
    Initial code, will likely need to be refactored

--- src/all-page.js
+++ src/all-page.js
@@ -7,10 +7,13 @@ export function loadAll() {
     const title = document.createElement("h3");
     title.textContent = "All Todos";
     mainDiv.appendChild(title);
+    const todoContainer = document.createElement("div");
+    todoContainer.classList.add("todo-container");
+    mainDiv.appendChild(todoContainer);
     const addTodoPageBtn = document.createElement("button");
     addTodoPageBtn.classList.add("add-todo-page-button");
     addTodoPageBtn.textContent = `+ Add Todo`;
-    if (allTodos[0] === undefined) {
+    if (allTodos.length === 0) {
         const todosTextDiv = document.createElement("div");
         todosTextDiv.classList.add("todos-text-div");
         const todosTextLine1 = document.createElement("p");
@@ -19,7 +22,27 @@ export function loadAll() {
         const todosTextLine2 = document.createElement("p");
         todosTextLine2.textContent = "\uD83D\uDCDD\u00A0 Add one now to get started!";
         todosTextDiv.appendChild(todosTextLine2);
-        mainDiv.appendChild(todosTextDiv);
+        todoContainer.appendChild(todosTextDiv);
         mainDiv.appendChild(addTodoPageBtn);
-    };
+    } else {
+        mainDiv.insertBefore(addTodoPageBtn, todoContainer);
+        for (const todo of allTodos) {
+            const todoDiv = document.createElement("div");
+            todoDiv.classList.add("todo-div");
+            const title = document.createElement("p");
+            title.textContent = todo.title;
+            const description = document.createElement("p");
+            description.textContent = todo.description;
+            const dueDate = document.createElement("p");
+            dueDate.textContent = `Due: ${todo.dueDate}`;
+            const priority = document.createElement("p");
+            priority.textContent = `Priority: ${todo.priority}`;
+            const projectName = document.createElement("p");
+            projectName.textContent = `Project: ${todo.project}`;
+            const isComplete = document.createElement("p");
+            isComplete.textContent = `Is complete: ${todo.isComplete}`;
+            todoDiv.append(title, description, dueDate, priority, projectName, isComplete);
+            todoContainer.appendChild(todoDiv);
+        }
+    }
 }
