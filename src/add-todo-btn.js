0e473e0c3d2a7d7cdba54e2653df506a438cb56a
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-03-26 13:08:48 -0400

    Add date picker to 'add-todo' dialog

--- src/add-todo-btn.js
+++ src/add-todo-btn.js
@@ -8,12 +8,12 @@ export function activateAddTodoButton() {
     const dialogProject = addTodoDialog.querySelector("#project");
 
     addTodoBtn.addEventListener("click", () => {
-        addTodoDialog.showModal();
         dialogTodoName.value = "";
         dialogDescription.value = "";
         dialogDueDate.value = "";
         dialogPriority.value = "2 - Normal";
         dialogProject.value = "Personal";
         addTodoDialog.returnValue = "";
+        addTodoDialog.showModal();
     })
 }
--- src/index.js
+++ src/index.js
@@ -94,9 +94,9 @@ console.log(projects.getAllProjects());
 
 todos.addTodo('Call Mom', 'make sure to call Mom, she worries', new Date('2025-03-12T00:00:00'), 1, 'personal', 'no');
 
-todos.addTodo('Do Laundry', 'do your laundry you heathen', new Date('2025-03-29T00:00:00'), 3, 'personal', 'no');
+todos.addTodo('Do Laundry', 'do your laundry you heathen', new Date('2025-03-28T00:00:00'), 3, 'personal', 'no');
 
-todos.addTodo('Meeting with Joe', 'discuss important business', new Date('2025-03-25T00:00:00'), 1, 'work', 'no');
+todos.addTodo('Meeting with Joe', 'discuss important business', new Date('2025-03-26T00:00:00'), 1, 'work', 'no');
 
 todos.addTodo(`Shoot 100 3's`, 'gotta improve that stroke', new Date('2025-03-29T00:00:00'), 2, 'hobbies', 'no');
 
