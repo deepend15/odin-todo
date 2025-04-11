7b7ef16cb7e1657ba126cd12b4d1440291e65231
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-04-11 17:28:00 -0400

    Add missing plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,12 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
     devtool: "source-map",
+    plugins: [new MiniCssExtractPlugin()],
     module: {
         rules: [
             {
